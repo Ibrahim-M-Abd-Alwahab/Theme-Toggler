13fb0390b8e82d219e563214030b2c944e297f51
ibrahim-mohammed-99 <ibrabimmoh2@gmail.com>
2021-10-20 12:30:54 +0200

    initial commit

---
+++ assest/js/script.js
@@ -0,0 +1,31 @@
+let themeToggler = document.querySelector(".theme-toggler");
+
+themeToggler.addEventListener('click', () => {
+    themeToggler.classList.toggle('active');
+    if (themeToggler.classList.contains('active')) {
+        document.body.classList.add('active');
+    } else {
+        document.body.classList.remove('active');
+    }
+});
+
+
+document.querySelectorAll('.theme-colors .color').forEach(color => {
+    color.addEventListener('click', () => {
+        let background = color.style.background;
+        document.querySelector(':root').style.setProperty('--main-color', background)
+    });
+});
+
+let theme = document.querySelector('.themes-container');
+
+
+document.querySelector('#theme-open').addEventListener('click', () => {
+    theme.classList.add('active');
+    document.body.style.paddingRight = '350px';
+});
+
+document.querySelector('#theme-close').addEventListener('click', () => {
+    theme.classList.remove('active');
+    document.body.style.paddingRight = '0';
+});
